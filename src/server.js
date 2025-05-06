7fc1b454cec1a5bd5d2fac93ddd7509136e764f1
Qonye <konyemartin@outlook.com>
2025-05-06 15:57:48 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:5000',
+    'https://safiritickets.netlify.app',
     'http://127.0.0.1:5000',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5500', // Live server via IP
