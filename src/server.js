7ec37ab4c059336e6f10cb70d41f5e42a1ae23a0
Qonye <konyemartin@outlook.com>
2025-05-13 14:27:25 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(cors({
     'http://localhost:3000',
     'https://safiritickets.netlify.app',
     'http://127.0.0.1:5000',
-    'https://admin.safirii.io',
+    'https://admin.safiritickets.com',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5500', // Live server via IP
     'http://localhost:5500',  // Live server via localhost
